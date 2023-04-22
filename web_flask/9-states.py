0c8eb866abde8820af6e3768f22180fad831279e
David Egwuatu <davidegwuatu2@gmail.com>
2023-04-22 13:31:24 +0000

    resolved task 11 error(viii)

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
 """
+Flask web app
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states", strict_slashes=False)
+@app.route('/states')
 def states():
-    """Displays an HTML page with a list of all States.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
+    """ Returns the states created """
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', items=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+@app.route('/states/<state_id>')
+def states():
+    """ Returns the states created """
+    for state in storage.all('State').values():
+        if state.id == state_id:
+            return render_template('8-cities_by_states.html', items=state)
+    return render_template('8-cities_by_states.html', items=state)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exception):
+    """ tearsdown connection """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
